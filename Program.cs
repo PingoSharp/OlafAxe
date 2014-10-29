cf81ba23a6728e5320ff4c70c95c54c1efe01556
Angelo S. <boyangel90@hotmail.it>
2014-10-29 17:36:56 +0100

    OlafAxe

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+
+namespace OlafAxe
+{
+    class Program
+    {
+        private static GameObject _axeObj;
+
+        static void Main(string[] args)
+        {
+            Drawing.OnDraw += Drawing_OnDraw;
+            GameObject.OnCreate += GameObject_OnCreate;
+            GameObject.OnDelete += GameObject_OnDelete;
+        }
+
+        private static void GameObject_OnCreate(GameObject obj, EventArgs args)
+        {
+            if (obj.Name.Equals("olaf_axe_totem_team_id_green.troy"))
+                _axeObj = obj;
+        }
+
+        private static void GameObject_OnDelete(GameObject obj, EventArgs args)
+        {
+            if (obj.Name.Equals("olaf_axe_totem_team_id_green.troy"))
+                _axeObj = null;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (_axeObj != null)
+            {
+                var screenCoord = Drawing.WorldToScreen(_axeObj.Position);
+                Drawing.DrawText(screenCoord.X, screenCoord.Y, Color.BlueViolet, "AXEEE");
+            }
+        }
+    }
+}
