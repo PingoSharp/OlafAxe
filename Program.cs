b92735bbeb1151ba15356698cea833d81d7f8043
Angelo S. <boyangel90@hotmail.it>
2014-10-29 23:40:48 +0100

    sssssss

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ namespace OlafAxe
             {
                 var screenCoord = Drawing.WorldToScreen(_axeObj.Position);
                 Drawing.DrawText(screenCoord.X, screenCoord.Y, Color.Yellow, "AXEEE");
-                Utility.DrawCircle(_axeObj.Position, 100, Color.Yellow);
+                Utility.DrawCircle(_axeObj.Position, 100, Color.Yellow, 2);
             }
         }
     }
